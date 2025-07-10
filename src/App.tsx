697f1f7ae1dd7afda01205aa1c439e028485673e
Naitik Gupta <guptanaitik46@gmail.com>
2025-07-10 14:27:20 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from './components/Header';
 import AuthPage from './pages/AuthPage';
 import TestPage from './pages/TestPage';
 import FeedbackPage from './pages/FeedbackPage';
-import { TestProvider } from './context/TestContext';
+import { TestProvider } from './Context/TestContext';
 
 function App() {
   return (
